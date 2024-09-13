3e108e0ccfef7b7f7e9ae45c94542ef472aead8f
yash013 <yashsuthar013@gmail.com>
2024-09-13 20:56:54 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -1,21 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
-// const socketIo = require('socket.io');
 const app = express();
-const server = http.createServer(app);
 const cors = require('cors');
 
 const corsOptions = {
   origin: ['https://task-man-pi.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
 const usersRoute = require("./routes/usersRoute");
@@ -29,16 +25,11 @@ app.use("/api/tasks", tasksRoute);
 app.use("/api/notifications", notificationsRoute);
 
 const dbConfig = require("./config/dbConfig");
-const port = process.env.PORT || 5000;
 
 // Serve static files from the React app
 const path = require("path");
 __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
@@ -50,10 +41,10 @@ dbConfig.connection.on('connected', () => {
   console.log('MongoDB connected');
 });
 
-// dbConfig.connection.on('error', (err) => {
-//   console.error('MongoDB connection error: ', err);
-//   process.exit(1);
-// });
+dbConfig.connection.on('error', (err) => {
+  console.error('MongoDB connection error: ', err);
+  process.exit(1);
+});
 
 // Uncomment and use this if you need to use io in other files
 // module.exports = { io };
