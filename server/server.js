975c7efe3d44a027c8e40e6b15e1c9a85ee91892
yash013 <yashsuthar013@gmail.com>
2024-09-13 21:38:26 +0530

    simple changes

--- server/server.js
+++ server/server.js
@@ -1,20 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
-// const socketIo = require('socket.io');
+const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
+// const io = socketIo(server);
+// console.log(process.env);
 const cors = require('cors');
-
-const corsOptions = {
-  origin: ['http://task-man-pi.vercel.app', 'https://task-man-pi.vercel.app','http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
+const io = socketIo(server, {
+  cors: {
+    origin: "https://task-man-pi.vercel.app/" , // Use environment variable for client URL
+    methods: ['GET', 'POST'],
+  },
+});
 
 app.use(express.json());
+
+const dbConfig = require("./config/dbConfig");
+// const port = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: 'https://task-man-pi.vercel.app/',
+  methods: ['GET', 'POST', 'UPDATE', 'DELETE'] // or your client's URL
+}));
+
+// console.log("MongoDB_URI:", process.env.MONGODB_URI);
 
 const usersRoute = require("./routes/usersRoute");
 const projectsRoute = require("./routes/projectsRoute");
@@ -26,32 +36,35 @@ app.use("/api/projects", projectsRoute);
 app.use("/api/tasks", tasksRoute);
 app.use("/api/notifications", notificationsRoute);
 
-const dbConfig = require("./config/dbConfig");
-const port = process.env.PORT || 5000;
-
 // Serve static files from the React app
 const path = require("path");
-__dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'client/build')));
+dirname = path.resolve();
+app.use(express.static(path.join(dirname, 'client/build')));
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
 });
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(dirname, "client", "build", "index.html"));
   });
 }
+
+// server.listen(port, () => {
+//   console.log(Server is running on port ${port});
+// });
 
 dbConfig.connection.on('connected', () => {
   console.log('MongoDB connected');
 });
 
-// dbConfig.connection.on('error', (err) => {
-//   console.error('MongoDB connection error: ', err);
-//   process.exit(1);
-// });
-
-// Uncomment and use this if you need to use io in other files
-// module.exports = { io };
+dbConfig.connection.on('error', (err) => {
+  console.error('MongoDB connection error: ', err);
+  process.exit(1); // Exit the process with an error code
+});
