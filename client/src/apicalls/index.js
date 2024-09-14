98192448f4e70f041a020fe3d17fae0e6ffe45f1
yash013 <yashsuthar013@gmail.com>
2024-09-15 00:30:16 +0530

    added url

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: SERVER_URL,
 });
 
 export const apiRequest = async (method, url, payload) => {
