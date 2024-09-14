8e3b90ad93b1144aa0e8ed13d108a30eeed636c1
yash013 <yashsuthar013@gmail.com>
2024-09-15 00:36:48 +0530

    minor error solved

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: SERVER_URL,
+  baseURL: process.env.SERVER_URL,
 });
 
 export const apiRequest = async (method, url, payload) => {
