15e676d2e13cff7450400632f9da585423e78125
Yash Suthar <75071915+yash013@users.noreply.github.com>
2024-09-16 21:13:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,11 +17,11 @@ const io = socketIo(server, {
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
